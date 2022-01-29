9822d42eb7cfe1a5212b0c5d4388d1a351f156c7
KASHISH <kd372744@gmail.com>
2022-01-29 21:47:35 +0530

    Project Start ToDolist App
    make a web application to do list  with user authentication , made  with django framework.

---
+++ testProject/todo_list/base/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Tasks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    task_status = models.BooleanField(default=False)
+    created_dt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['task_status']
